e56e004ffbb0214bd63f29a06fabcb70a93eb5b0
Matheus Pereira <mathp874@gmail.com>
2024-09-03 13:43:30 -0300

    Primeiro commit

---
+++ Indice_ex.py
@@ -0,0 +1,12 @@
+# Definindo as variáveis
+indice = 12
+soma = 0
+k = 1
+
+# Loop enquanto k for menor que o índice
+while k < indice:
+    k = k + 1
+    soma = soma + k
+
+# Imprimindo o resultado
+print(soma)
